e97d379e00b84500366a83187be6c5f6335909c4
Mattia Gallinaro <m.gallinaro@exabytesrl.it>
2026-02-26 17:03:44 +0100

    Moved background effect from login page to App level

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,13 +2,14 @@
 // #endregion
 
 // #region [Libraries Imports]
+import { useEffect } from "react";
 import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router/dom"
 import { Toaster } from "@/components/ui/toaster";
 // #endregion
 
 // #region [Styles Imports]
-import "./App.css"
+import styles from "./App.module.css"
 // #endregion
 
 // #region [Pages Imports]
@@ -24,13 +25,19 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />
-  },{
+  }, {
     path: "/dashboard",
     element: <ProtectedRoute requiredRole="basic"><Dashboard /></ProtectedRoute>,
   }
 ]);
 
 export const App = () => {
+  // Add gradient background class to body on mount and remove it on unmount
+  useEffect(() => {
+    document.body.classList.add(styles['gradient-background'])
+    return () => { document.body.classList.remove(styles['gradient-background']) }
+  }, [])
+  
   return (
     <>
       <RouterProvider router={router} />
